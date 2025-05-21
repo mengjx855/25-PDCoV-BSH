3f95904a6b52c032d2f6c8397163337a37c536f5
有轻轨的明故城 <78715559+mengjx855@users.noreply.github.com>
2025-05-21 16:07:18 +0800

    2_update

--- MBX-nonTarget/mjx_analysis.R
+++ MBX-nonTarget/mjx_analysis.R
@@ -1,107 +1,90 @@
-#### Jinxin Meng, 20250327, 20250415 ####
-setwd("F:/project/20250313_PDCoV_BAC_Bile_Zhangyq/MBX-nonTarget/")
+#### Jinxin Meng, 20250327, 20250518 ####
+setwd('F:/project/20250313_PDCoV_BAC_Bile_Zhangyq/data_available/statistics/MBX-nonTarget/')
 pacman::p_load(tidyr, dplyr, tibble, purrr, ggpubr, openxlsx)
 library(ropls)
 library(clusterProfiler)
-source("/code/R_func/difference_analysis.R")
-source("/code/R_func/calcu_diff.R")
+source('/code/R_func/difference_analysis.R')
+source('/code/R_func/calcu_diff.R')
 
 #### info ####
-grp <- c('Mock','PDCoV')
-grp_col <- structure(c('#a0a0a4','#d7b0b0'), names = grp)
+group_level <- c('Mock','PDCoV')
+group_color <- structure(c('#a0a0a4','#d7b0b0'), names = group_level)
 
-MBT_info <- read.delim("MBT_info.tsv")
-group <- read.delim("group.tsv")
-profile <- read.delim("MBT_profile.tsv", row.names = 1) %>% 
+MBT_info <- read.delim('MBT_info.tsv')
+group <- read.delim('group.tsv')
+profile <- read.delim('MBT_profile.tsv', row.names = 1) %>% 
   select(all_of(group$sample))
 
 #### PLS ####
-source("/code/R_func/plot_PLS.R")
+source('/code/R_func/plot_PLS.R')
 
-plot_PLS(profile, group, group_order = grp, group_color = grp_col,
-         add_group_label = T,  lab_size = 3, ellipse_level = .9, 
+plot_PLS(profile, group, group_level = group_level, group_color = group_color,
+         add_group_label = T,  label_size = 3, ellipse_level = .9, 
          show_legend = F, show_grid = T, show_line = F)
-ggsave("PLS.pdf", width = 4, height = 3)
+ggsave('PLS.pdf', width = 4, height = 3)
 
 #### diff ####
+# profile %>% 
+#   rownames_to_column('name') %>% 
+#   gather('sample', 'value', -name) %>% 
+#   mutate(.value = log10(value)) %>% 
+#   group_by(name) %>% 
+#   shapiro_test(.value)
+
 plsda <- opls(data.frame(t(log10(profile))), y = pull(group, group), orthoI = 0)
 diff <- difference_analysis(log10(profile), group, comparison = c('PDCoV', 'Mock'), 
                             method = 't')
 
 diff <- data.frame(plsda_vip = plsda@vipVn) %>% 
-  rownames_to_column("name") %>% 
-  left_join(diff, ., by = "name") %>% 
-  mutate(enriched = ifelse((plsda_vip > 1 | pval < 0.05) & log2FC > 0, "PDCoV", 
-                           ifelse((plsda_vip > 1 | pval < 0.05) & log2FC < 0, 
-                                  "Mock", "none"))) %>% 
+  rownames_to_column('name') %>% 
+  left_join(diff, ., by = 'name') %>% 
+  mutate(enriched = ifelse((plsda_vip > 1 & padj < 0.05) & log2FC > 0, 'PDCoV', 
+                           ifelse((plsda_vip > 1 & padj < 0.05) & log2FC < 0, 
+                                  'Mock', 'none'))) %>% 
   left_join(MBT_info, c('name' = 'cpd_id'))
-write.xlsx(diff, "diff.xlsx")
+write.xlsx(diff, 'diff.xlsx')
 
 #### volcano ####
-bile_related <- read.delim("/database/KEGG/enrichment_analysis/cpd2path_enrichment.tsv") %>% 
-  filter(grepl('bile', pathway)) %>% 
-  pull(ID) %>% 
-  unique()
-
-plot_label <- diff %>% 
-  filter(rowSums(sapply(bile_related, \(x) grepl(x, KEGG))) > 0) %>% 
-  filter(enriched != 'none')
-  
 plot_data <- diff %>% 
-  select(name, cpd_name, pval, log2FC, enriched, plsda_vip) %>% 
+  select(name, cpd_name, padj, log2FC, enriched, plsda_vip) %>% 
   mutate(.enriched = ifelse(is.na(enriched), 'none', enriched),
-         .pval = -log10(pval),
+         .padj = -log10(padj),
          .log2FC = log2FC,
          .log2FC = ifelse(.log2FC < -5, -5, .log2FC),
          .log2FC = ifelse(.log2FC > 2.5, 2.5, .log2FC),
-         .vip = ifelse(plsda_vip > 1, plsda_vip, .8),
-         .label = ifelse(name %in% plot_label$name, cpd_name, ''))
+         .vip = ifelse(plsda_vip > 1, plsda_vip, .8))
 
-ggscatter(plot_data, ".log2FC", ".pval", color = ".enriched", size = '.vip', 
-          xlab = 'log2FoldChange', ylab = "-log10 P-value",
-          palette = c(grp_col, 'none' = 'grey78')) +
+ggscatter(plot_data, '.log2FC', '.padj', color = '.enriched', size = '.vip', 
+          xlab = 'log2FoldChange', ylab = '-log10 BH-adjusted P-value', legend = 'right',
+          palette = c(group_color, 'none' = 'grey78')) +
   scale_size_continuous(range = c(1, 2.5)) +
-  geom_hline(yintercept = -log10(0.05), linetype = "dashed") +
-  geom_vline(xintercept = c(0), linetype = "dashed") +
+  geom_hline(yintercept = -log10(0.05), linetype = 'longdash') +
+  geom_vline(xintercept = c(0), linetype = 'longdash') +
   theme(aspect.ratio = 1)
 ggsave('diff.volcano.pdf', width = 5, height = 5)
 
-ggscatter(plot_data, ".log2FC", ".pval", color = ".enriched", size = '.vip', 
-          xlab = 'log2FoldChange', ylab = "-log10 P-value",
-          palette = c(grp_col, 'none' = 'grey78')) +
-  scale_size_continuous(range = c(1, 2.5)) +
-  geom_text(aes(label = .label), size = 1) +
-  geom_hline(yintercept = -log10(0.05), linetype = "dashed") +
-  geom_vline(xintercept = c(0), linetype = "dashed") +
-  theme(aspect.ratio = 1)
-ggsave('diff.volcano.bile.pdf', width = 5, height = 5)
-
 #### enrich ####
-path_info <- read.delim("cpd2path_enrichment.tsv")
+path_info <- read.delim('cpd2path_enrichment.tsv')
 
 eKEGG <- diff %>% 
   filter(enriched != 'none') %>% 
-  filter(KEGG != "") %>% 
+  filter(KEGG != '') %>% 
   pull(KEGG) %>% 
-  map(~ unlist(strsplit(.x, ";"))) %>% 
+  map(~ unlist(strsplit(.x, ';'))) %>% 
   flatten_chr() %>% 
   enricher(TERM2GENE = path_info, minGSSize = 1, pvalueCutoff = 1, qvalueCutoff = 1) %>% 
   data.frame
-write.xlsx(eKEGG, "diff.eKEGG.xlsx")
+write.xlsx(eKEGG, 'diff.eKEGG.xlsx')
 
-kegg_info <- read.delim('path_description') %>% 
-  filter(category == 'Metabolism')
-
-path_name <- c('map00997', 'map01040', 'map01230', 'map00120', 'map00920', 
-               'map01200', 'map00400', 'map00121', 'map00310', 'map00100',
-               'map00350', 'map00460', 'map00270')
+path_name <- c('map00120', 'map00401', 'map00300', 'map00400', 'map00121', 'map00997', 
+               'map00460', 'map00140', 'map00330', 'map00470', 'map00740', 'map00220')
 
 plot_data <- eKEGG %>%
   mutate(path = map_vec(strsplit(ID, ':'), \(x) x[1])) %>% 
   filter(path %in% path_name) %>% 
   arrange(FoldEnrichment)
 
-ggscatter(plot_data, "ID", "FoldEnrichment", fill = "pvalue", 
+ggscatter(plot_data, 'ID', 'FoldEnrichment', fill = 'pvalue', 
           rotate = T, size = 'FoldEnrichment',
           shape = 21, legend = 'right', xlab = '', ylab = 'Fold Enrichment') +
   scale_fill_viridis_c(begin = .6) +
@@ -109,4 +92,3 @@ ggscatter(plot_data, "ID", "FoldEnrichment", fill = "pvalue",
   theme(aspect.ratio = 2, 
         panel.grid.major = element_line(linewidth = .5, color = 'grey88'))
 ggsave('diff.eKEGG.pdf', width = 8, height = 4.5)
-
